f46ff13b152764847b62bd75b29d9340c2a12292
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 00:04:01 +0530

    Added private attributes

--- Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/domain/Employee.java
+++ Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/domain/Employee.java
@@ -8,4 +8,12 @@
 package chaurasiaaditya.Employee_Manager.domain;
 
 public class Employee {
+	private Long id;
+	private String name;
+	private String email;
+	private String jobTitle;
+	private String phone;
+	private String imageUrl;
+	private String employeeCode;
+
 }
