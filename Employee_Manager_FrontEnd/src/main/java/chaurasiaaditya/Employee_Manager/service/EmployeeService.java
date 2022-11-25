e42f86ea748830e8f0bdbabbb361f73d85c48633
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 00:49:10 +0530

    added method in interface

--- Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/service/EmployeeService.java
+++ Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/service/EmployeeService.java
@@ -1,4 +1,18 @@
 package chaurasiaaditya.Employee_Manager.service;
 
+import chaurasiaaditya.Employee_Manager.domain.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface EmployeeService {
+	Employee addEmployee(Employee employee);
+
+	List<Employee> getAllEmployee();
+
+	Employee updateEmployee(Employee employee);
+
+	Optional<Employee> findById(long id);
+
+	String deleteEmployee(long id);
 }
