5d808eebfd6a27f87ccd90cacc6a809f41b33ed1
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-25 23:53:17 +0530

    Created Employee class

---
+++ Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/domain/Employee.java
@@ -0,0 +1,9 @@
+/*
+* Author Name: Aditya Chaurasia
+* Date: 25-11-2022
+* Created With: IntelliJ IDEA Ultimate
+* Profile: github.com/ChaurasiaAditya
+* Website: ChaurasiaAditya.in
+*/
+package chaurasiaaditya.Employee_Manager.domain;public class Employee {
+}
