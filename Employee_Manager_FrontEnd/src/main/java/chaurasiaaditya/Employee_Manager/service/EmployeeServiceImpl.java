b78d9800ef4964c5c688f3782458d13c7d6560b5
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 00:32:13 +0530

    Injected employee Repository and added Annotations

--- Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/service/EmployeeServiceImpl.java
+++ Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/service/EmployeeServiceImpl.java
@@ -7,5 +7,16 @@
  */
 package chaurasiaaditya.Employee_Manager.service;
 
+import chaurasiaaditya.Employee_Manager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeServiceImpl {
+	private final EmployeeRepository employeeRepository;
+
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
 }
