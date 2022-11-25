20a50eb8cc1b181255b3b48b1cac965905137bee
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 00:28:51 +0530

    exteds to JpaRepository

--- Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/repository/EmployeeRepository.java
+++ Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/repository/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package chaurasiaaditya.Employee_Manager.repository;
 
-public interface EmployeeRepository {
+import chaurasiaaditya.Employee_Manager.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
+
