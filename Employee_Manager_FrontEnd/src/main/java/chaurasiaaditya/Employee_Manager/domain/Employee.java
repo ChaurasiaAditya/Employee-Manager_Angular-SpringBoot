a0097a8f47b173dc6e2b7911bb1a15ba21d27da4
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 00:09:17 +0530

    Added Required annotations

--- Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/domain/Employee.java
+++ Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/domain/Employee.java
@@ -7,13 +7,21 @@
  */
 package chaurasiaaditya.Employee_Manager.domain;
 
-public class Employee {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
 	private Long id;
 	private String name;
 	private String email;
 	private String jobTitle;
 	private String phone;
 	private String imageUrl;
+	@Column(nullable = false, updatable = false)
 	private String employeeCode;
 
 }
