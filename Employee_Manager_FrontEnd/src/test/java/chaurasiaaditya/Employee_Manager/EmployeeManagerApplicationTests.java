38c78daca3fb86ea20ab75e5045f9f50025d6cae
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-25 23:44:03 +0530

    Initialise Spring Boot with required Dependencies

---
+++ Employee_Manager_FrontEnd/src/test/java/chaurasiaaditya/Employee_Manager/EmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package chaurasiaaditya.Employee_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
