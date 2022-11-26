884903a1c07a4fdd89d2c1ca99e859f9df13ed7e
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 13:02:43 +0530

    injected Employe service in controller

--- Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/controller/EmployeeController.java
+++ Employee_Manager_FrontEnd/src/main/java/chaurasiaaditya/Employee_Manager/controller/EmployeeController.java
@@ -7,5 +7,18 @@
  */
 package chaurasiaaditya.Employee_Manager.controller;
 
+import chaurasiaaditya.Employee_Manager.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
 public class EmployeeController {
+	private final EmployeeServiceImpl employeeService;
+
+	@Autowired
+	public EmployeeController(EmployeeServiceImpl employeeService) {
+		this.employeeService = employeeService;
+	}
 }
