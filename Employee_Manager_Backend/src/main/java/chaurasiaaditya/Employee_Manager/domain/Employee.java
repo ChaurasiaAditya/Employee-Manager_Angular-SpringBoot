6f18980e521388d5727e14cd5518b6ba523f281c
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 23:27:58 +0530

    formatted the code

--- Employee_Manager_Backend/src/main/java/chaurasiaaditya/Employee_Manager/domain/Employee.java
+++ Employee_Manager_Backend/src/main/java/chaurasiaaditya/Employee_Manager/domain/Employee.java
@@ -37,6 +37,14 @@ public class Employee implements Serializable {
 		this.employeeCode = employeeCode;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
--- Employee_Manager_FrontEnd/src/app/app.module.ts
+++ Employee_Manager_FrontEnd/src/app/app.module.ts
@@ -5,20 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashBoardComponent } from './dash-board/dash-board.component';
+import { DashBoardComponent } from "./dash-board/dash-board.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     DashBoardComponent
   ],
+  providers: [],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  ]
 })
 export class AppModule { }
