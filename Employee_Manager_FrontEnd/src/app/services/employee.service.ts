416b8379e0c0da5b770a4b94b283af1258f2f102
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-26 15:10:20 +0530

    added CRUD methods to service.ts

--- Employee_Manager_FrontEnd/src/app/services/employee.service.ts
+++ Employee_Manager_FrontEnd/src/app/services/employee.service.ts
@@ -1,10 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../model/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+
   private apiUrl = 'http://localhost:8080/api/employees';
+
   constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiUrl}/api/v1/all`);
+  }
+
+  public addEmployees(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiUrl}/api/v1/add`, employee);
+  }
+
+  public updateEmployees(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiUrl}/api/v1/update`, employee);
+  }
+
+  public deleteEmployees(employeeId: number): Observable<Employee> {
+    return this.http.delete<Employee>(`${this.apiUrl}/api/v1/delete/${employeeId}`);
+  }
+
 }
